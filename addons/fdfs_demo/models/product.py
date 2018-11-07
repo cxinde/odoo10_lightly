ee62330c66cc75808168497f01bf42333f82fd3f
tedi3231 <tedi3231@gmail.com>
2018-11-07 14:22:13 +0800

    now we can display image in list

--- addons/fdfs_demo/models/product.py
+++ addons/fdfs_demo/models/product.py
@@ -7,3 +7,12 @@ class Product(models.Model):
 
     name = fields.Char(string='Name', required=True)
     pic_url = fields.Char(string='PicUrl')
+    line_ids = fields.One2many('fdfs_demo.product.line', 'product_id', string='Lines')
+
+    
+class ProductLine(models.Model):
+    _name = 'fdfs_demo.product.line'
+
+    product_id = fields.Many2one('fdfs_demo.product', string='Product', required=True)
+    name = fields.Char(string='Line')
+    url = fields.Char(string='URL')
